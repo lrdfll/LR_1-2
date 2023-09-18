aa9bbeaeea0888d33c33b25f56fbfd41d5953e53
lrdfll <144678075+lrdfll@users.noreply.github.com>
2023-09-18 15:49:16 +0300

    Update 18.1.pas

--- 18.1.pas
+++ 18.1.pas
@@ -3,7 +3,7 @@ var a: integer;
 begin 
   write('Введите номер текущего года ');
   readln(a);
-  if ((a mod 4 = 0) and (a mod 100<>0)) or (a mod 400 = 0)  then 
+  if ((a mod 4 = 0) and (a mod 100<>0)) then 
       write('Високосный год')
   else write('Год не високосный');
 end.
